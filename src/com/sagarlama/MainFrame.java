bbf33e8c7d755ee00d37ca4944c6b1913cdb4612
Sagar Lama <sagarlama826@gmail.com>
2018-12-04 13:51:01 +0545

    MainFrame added

---
+++ src/com/sagarlama/MainFrame.java
@@ -0,0 +1,13 @@
+package com.sagarlama;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        super("College Project");
+        setMinimumSize(new Dimension(500,600));
+        setSize(600,600);
+        setVisible(true);
+    }
+}
