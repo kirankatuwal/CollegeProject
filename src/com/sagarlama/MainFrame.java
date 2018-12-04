090f822cfb7190c34041b45ad42f1483fd4b0d73
Sagar Lama <sagarlama826@gmail.com>
2018-12-04 14:21:26 +0545

    Project Setup

--- src/com/sagarlama/MainFrame.java
+++ src/com/sagarlama/MainFrame.java
@@ -6,8 +6,19 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     public MainFrame(){
         super("College Project");
+
+
+
+
+
+
+
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(500,600));
         setSize(600,600);
         setVisible(true);
     }
 }
+
